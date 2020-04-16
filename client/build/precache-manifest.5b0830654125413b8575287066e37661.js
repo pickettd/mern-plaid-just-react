3f29637832b588839dc0c34c84a4b0f7d56bad37
David Pickett <pickettd@gmail.com>
2020-04-15 16:40:44 -1000

    first commit

---
+++ client/build/precache-manifest.5b0830654125413b8575287066e37661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6ae32176b92c2fd029ed75b9801e8ce",
+    "url": "/static/media/credit_card.d6ae3217.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dbc26f53150d85422b30",
+    "url": "/static/js/main.dbc26f53.chunk.js"
+  },
+  {
+    "revision": "da8286fefc193bbe7996",
+    "url": "/static/js/1.da8286fe.chunk.js"
+  },
+  {
+    "revision": "dbc26f53150d85422b30",
+    "url": "/static/css/main.3a775999.chunk.css"
+  },
+  {
+    "revision": "76334906d04ee05271cad32356c967c9",
+    "url": "/index.html"
+  }
+];
---
+++ client/build/precache-manifest.7acf9e3eeed391109b607eacee2fe5c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6ae32176b92c2fd029ed75b9801e8ce",
+    "url": "/static/media/credit_card.d6ae3217.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b63a60e5546c02ad8660",
+    "url": "/static/js/main.b63a60e5.chunk.js"
+  },
+  {
+    "revision": "da8286fefc193bbe7996",
+    "url": "/static/js/1.da8286fe.chunk.js"
+  },
+  {
+    "revision": "b63a60e5546c02ad8660",
+    "url": "/static/css/main.3a775999.chunk.css"
+  },
+  {
+    "revision": "2fe5c4a028dd0409d31069218bf31505",
+    "url": "/index.html"
+  }
+];
